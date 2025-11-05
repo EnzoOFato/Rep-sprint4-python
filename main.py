994b79c89ba4d03797683ad2bf0b19b5ad8caf71
Enzo Fatobene <enzoamafatobene@gmail.com>
2025-11-05 00:14:30 -0300

    case condition

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
+# todos imports para o funcionamento do programa
 import os
 from mensagens import *
 from contagem import Contagem
 from crud import *
 
+# declaração do objeto contagem, que será usado para contagem regressiva de 3 segundos
 contagem = Contagem(3)
 
 while True:
-    os.system("cls")
+    os.system("cls") # comando para limpar o cmd
     escolha = mensagem_incial()
-    if escolha == 1:
-        consultar_jogadoras_alfa()
-        input(continuar())
-    elif escolha == 2:
-        consultar_jogadora_id()
-        input(continuar())
-    elif escolha == 3:
-        cadastrar_jogadora()
-        contagem.contar()
-    elif escolha == 4:
-        atualizar_jogadora()
-        contagem.contar()
-    elif escolha == 5:
-        deletar_jogadora()
-        contagem.contar()
-    elif escolha == 6:
-        consultar_jogadora_id()
-        input(continuar())
-    elif escolha == 7:
-        mensagem_final()
-        contagem.contar()
-        break
-    else:
-        mensagem_erro()
+    match escolha: 
+        case 1:
+            consultar_jogadoras_alfa()
+            input(continuar())
+        case 2:
+            consultar_jogadora_id()
+            input(continuar())
+        case 3:
+            cadastrar_jogadora()
+            contagem.contar()
+        case 4:
+            atualizar_jogadora()
+            contagem.contar()
+        case 5:
+            deletar_jogadora()
+            contagem.contar()
+        case 6:
+            consultar_jogadora_id()
+            input(continuar())
+        case 7:
+            mensagem_final()
+            contagem.contar()
+            break
+        case _:
+            mensagem_erro()
